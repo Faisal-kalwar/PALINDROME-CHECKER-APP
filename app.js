b25667e98e10aa00440fe4d7d1ff86f357deffe8
Faisal kalwar <faisalkalwar48@gmail.com>
2021-06-25 14:32:27 +0500

    Palindrome checker app completed

---
+++ app.js
@@ -0,0 +1,24 @@
+function palindrome()  
+{  
+var a= document.getElementById("pa").value;
+results = document.getElementById("pa2");
+
+a = a.toLowerCase();  
+var b = "";  
+for (i = a.length-1; i >= 0; i--)  
+{  
+b = b + a[i]
+b = b.toLowerCase(); 
+}
+if (a == ''){
+    alert("Please enter the string");
+}  
+else if (a == b){  
+results.innerHTML = b + "- ✔ is a Palindrome String";
+results.style.color = "green";
+}
+else{; 
+results.innerHTML = b + "- ✖ is not a Palindrome String";
+results.style.color = "red";
+}
+}  
